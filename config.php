8a99639595508c09b2b2934337d31c6674f32a06
hackronshins <jdprasadv@gmail.com>
2016-11-13 04:40:46 +0530

    opencart website files

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/pav_flashshop_quickstart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/pav_flashshop_quickstart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/pav_flashshop_quickstart/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/pav_flashshop_quickstart/system/');
+define('DIR_IMAGE', 'C:/wamp/www/pav_flashshop_quickstart/image/');
+define('DIR_LANGUAGE', 'C:/wamp/www/pav_flashshop_quickstart/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/pav_flashshop_quickstart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/pav_flashshop_quickstart/system/config/');
+define('DIR_CACHE', 'C:/wamp/www/pav_flashshop_quickstart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/pav_flashshop_quickstart/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/pav_flashshop_quickstart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/pav_flashshop_quickstart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/pav_flashshop_quickstart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
